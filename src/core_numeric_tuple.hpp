01cad565f22bbdb48cd27ad6cd725401e03db5ed
Jonathan Zrake <jonathan.zrake@gmail.com>
2020-03-18 17:28:21 -0400

    Function naming

--- src/core_numeric_tuple.hpp
+++ src/core_numeric_tuple.hpp
@@ -28,6 +28,7 @@
 
 #pragma once
 #include <tuple>
+#include <functional>
 
 
 
@@ -38,7 +39,7 @@ namespace numeric {
 namespace detail {
 
 template<typename... Ts, typename... Us, std::size_t... Is>
-auto zip_tuple_impl(std::tuple<Ts...> t, std::tuple<Us...> u, std::index_sequence<Is...>)
+auto zip_tuples_impl(std::tuple<Ts...> t, std::tuple<Us...> u, std::index_sequence<Is...>)
 {
     return std::tuple(std::pair(std::get<Is>(t), std::get<Is>(u))...);
 }
@@ -46,7 +47,7 @@ auto zip_tuple_impl(std::tuple<Ts...> t, std::tuple<Us...> u, std::index_sequenc
 template<typename... Ts, typename... Us>
 auto zip_tuples(std::tuple<Ts...> t, std::tuple<Us...> u)
 {
-    return zip_tuple_impl(t, u, std::make_index_sequence<sizeof...(Ts)>());
+    return zip_tuples_impl(t, u, std::make_index_sequence<sizeof...(Ts)>());
 }
 
 template<typename FunctionType, typename... Ts>
